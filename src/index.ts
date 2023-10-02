feec5e4b233d481d98f66ef69cfcff5f77f55b91
Will <williamneal1@proton.me>
2023-10-02 16:30:21 +0200

    feat:
    Added route logging to aid debugging when DEBUG environment variable is set

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import app from './app.js';
+import { parseExpressApp } from 'express-route-parser';
 
 try {
-    app.listen(4000, (): void => {
-        console.log('Server is listening on port 4000');
+    app.listen(4000, (): void => { 
+      console.log('Server is listening on port 4000');
+   
       });
     } catch (error) {
         if (error instanceof Error) {
             console.error(`Error occured: ${error.message}`);
          }
     }
+
+if (process.env.DEBUG) {
+  const parsed = parseExpressApp(app);
+  console.log('parsed', parsed);
+}
+
+
+
